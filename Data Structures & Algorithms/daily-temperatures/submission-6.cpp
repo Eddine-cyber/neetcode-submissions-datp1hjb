89303b653b32ba310c5e47eac923d22d46de7721
Salah <142009255+Eddine-cyber@users.noreply.github.com>
2026-04-09 21:18:32 +0200

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> temps_heap;
+        vector<int> res(temperatures.size());
+        temps_heap.push_back(0);
+        for(int i = 1; i<temperatures.size(); i++){
+            while(temperatures[temps_heap.back()] < temperatures[i] && temps_heap.size() > 0){
+                res[temps_heap.back()] = i - temps_heap.back();
+                temps_heap.pop_back();
+            }
+            temps_heap.push_back(i);
+        }
+
+        return res;
+
+    }
+};
