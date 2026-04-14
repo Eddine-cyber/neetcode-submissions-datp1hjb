55e45aebdb5d6257513b9aa9b4e56a51c7949f08
Salah <142009255+Eddine-cyber@users.noreply.github.com>
2026-04-14 20:31:21 +0200

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int DiameterOfBinaryTree(TreeNode root) {
+        if(root is null) return 0;
+        Stack<Tuple<TreeNode, int>> stack = new Stack<Tuple<TreeNode, int>>();
+        int rootDiametre = diametreNode(root);
+        stack.Push(new Tuple<TreeNode, int> (root, rootDiametre));
+        int diametre = rootDiametre;
+        while(stack.Count > 0){
+            Tuple<TreeNode, int> current = stack.Pop();
+            TreeNode currentNode = current.Item1;
+            if(currentNode is not null){
+                int rightDiameter = diametreNode(currentNode.right);
+                int leftDiameter = diametreNode(currentNode.left);
+                stack.Push(new Tuple<TreeNode, int> (currentNode.right, rightDiameter));
+                stack.Push(new Tuple<TreeNode, int> (currentNode.left, leftDiameter));
+                diametre = Math.Max(diametre, Math.Max(rightDiameter, leftDiameter));
+            }
+        } 
+        return diametre;
+    }
+
+    public int rightHight(TreeNode root){
+        if(root is null || root.right is null) return 0;
+        return 1 + Math.Max(rightHight(root.right) , leftHight(root.right));
+    }
+    public int leftHight(TreeNode root){
+        if(root is null || root.left is null) return 0;
+        return 1 + Math.Max(rightHight(root.left) , leftHight(root.left));
+    }
+
+    public int diametreNode(TreeNode root){
+        return rightHight(root) + leftHight(root);
+    }
+}
