12361abdcfe0c5bdd48537c2e223d247593d5483
Salah <142009255+Eddine-cyber@users.noreply.github.com>
2026-04-14 13:23:04 +0200

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cs
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public void ReorderList(ListNode head) {
+        int lenght = 0;
+        var real_head = head;
+        while(head is not null){
+            head = head.next;
+            lenght++;
+        }
+        int i= 0;
+        var mid_head = real_head;
+        while(i < (lenght-1)/2){
+            mid_head = mid_head.next;
+            i+=1;
+        }
+        var current1 = mid_head;
+        mid_head = mid_head.next;
+        current1.next = null;
+        
+        ListNode new_mid_head = ReverseList(mid_head);
+
+        PrintList(real_head, "First half");
+        PrintList(mid_head, "Second half before reverse");
+        PrintList(new_mid_head, "Second half reversed");
+
+        ListNode current;
+        var new_head = real_head;
+        while(new_head is not null){
+            current  = new_head.next;
+            new_head.next =  new_mid_head;
+            new_head = new_head.next;
+            new_mid_head = current;
+        }
+        head = new_head;
+    }
+
+    public ListNode ReverseList(ListNode head) {
+        if(head is null){return head;}
+        var node = head;
+        ListNode prev = null;
+        var next = node.next;
+        while(node is not null){
+            node.next = prev;
+            prev = node;
+            node = next; 
+            if(next != null) next = next.next;
+        }
+        return prev;
+    }
+
+    public void PrintList(ListNode head, string name)
+    {
+        Console.Write(name + ": ");
+        while (head != null)
+        {
+            Console.Write(head.val + " -> ");
+            head = head.next;
+        }
+        Console.WriteLine("null");
+    }
+}
