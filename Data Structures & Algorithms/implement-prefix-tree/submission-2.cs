646e03363736a47e067766e334b47cd61c52c26a
Salah <142009255+Eddine-cyber@users.noreply.github.com>
2026-04-15 14:33:33 +0200

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cs
@@ -0,0 +1,62 @@
+public class PrefixTree {
+
+    Dictionary<char, PrefixTree> dict;
+    bool endOfWord;
+
+    public PrefixTree() {
+        dict = new Dictionary<char, PrefixTree>();
+        endOfWord = false;
+    }
+    
+    public void Insert(string word) {
+        char current;
+        int i = 0;
+        Dictionary<char, PrefixTree> currentDict = dict;
+        while(i < word.Length){
+            current = word[i];
+            PrefixTree newPrefixTree;
+            if(! currentDict.ContainsKey(current)){
+                newPrefixTree = new PrefixTree();
+                currentDict[current] = newPrefixTree;
+            }
+            else newPrefixTree = currentDict[current];
+
+            if(i == word.Length-1){
+                newPrefixTree.endOfWord = true;
+            }
+            currentDict = newPrefixTree.dict;
+            i++;
+        }
+        
+    }
+    
+    public bool Search(string word) {
+        char current;
+        int i = 0;
+        PrefixTree currentTree = this;
+        while(i < word.Length){
+            current = word[i];
+            if(! currentTree.dict.ContainsKey(current)){
+                return false;
+            }
+            currentTree = currentTree.dict[current];
+            i++;
+        }
+        return currentTree.endOfWord;   
+    }
+    
+    public bool StartsWith(string prefix) {
+        char current;
+        int i = 0;
+        PrefixTree currentTree = this;
+        while(i < prefix.Length){
+            current = prefix[i];
+            if(! currentTree.dict.ContainsKey(current)){
+                return false;
+            }
+            currentTree = currentTree.dict[current];
+            i++;
+        }
+        return true;
+    }
+}
